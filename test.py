f4fd59a6df7f6aefc8ce391374e86ea161d40617
andrewdabuilder <andrew@filterhealth.co>
2022-11-30 18:39:37 +0000

    git ignore updated due to venv

--- test.py
+++ test.py
@@ -1,3 +1,13 @@
 # Virtual Env Guide
 import pandas as pd
 import numpy as np
+
+
+
+# usecols allows us to select our choice of features
+movies_df=pd.read_csv('MovieData/movie.csv', usecols=['movieId','title'], dtype={'movieId':'int32','title':'str'})
+#print(movies_df.head())
+
+ratings_df=pd.read_csv('MovieData/rating.csv',
+    usecols=['userId', 'movieId', 'rating','timestamp'],dtype={'userId': 'int32', 'movieId': 'int32', 'rating': 'float32'})
+print(ratings_df.head())
